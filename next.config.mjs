685a554cb8321a52e5ef0bec9e868c0e3361283e
MK <mikakort007@gmail.com>
2025-06-09 17:11:57 -0400

    trying link again

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? './' : undefined, // ✅ changed
+  assetPrefix: isProd ? 'https://mikaelkortbaoui.dev' : undefined, // ✅ changed
   images: {
     unoptimized: true,
   },
