5512c60ba95418dd5083cdf494dcf400284030f1
MK <mikakort007@gmail.com>
2025-06-09 17:05:05 -0400

    leading slash

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? './' : undefined, // ✅ changed
+  assetPrefix: isProd ? '/' : undefined, // ✅ changed
   images: {
     unoptimized: true,
   },
