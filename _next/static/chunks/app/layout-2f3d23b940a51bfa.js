e247f762f5a4279cd0bb5065bc94a7eadcc08e45
MK <mikakort007@gmail.com>
2025-06-09 16:29:37 -0400

    Updates

---
+++ _next/static/chunks/app/layout-2f3d23b940a51bfa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},963:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,8346,23)),Promise.resolve().then(a.t.bind(a,347,23))},8346:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_ab497b"}}},e=>{var s=s=>e(e.s=s);e.O(0,[838,441,684,358],()=>s(963)),_N_E=e.O()}]);
