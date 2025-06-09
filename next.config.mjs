0a7a17d3b3ee1d2a4187f8d5a2501f4c3cae9dc4
MK <mikakort007@gmail.com>
2025-06-09 17:03:56 -0400

    modified mjs removed ts

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: isProd ? 'https://mikaelkortbaoui.dev' : undefined,
+  assetPrefix: isProd ? './' : undefined, // ✅ changed
   images: {
     unoptimized: true,
   },
