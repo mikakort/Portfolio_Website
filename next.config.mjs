463d903221b06c4245fea12761cccf7ed1d83a56
MK <mikakort007@gmail.com>
2025-06-10 10:22:48 -0400

    broke styling again

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const repo = 'Portfolio_Website';
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
 const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     unoptimized: true,
   },
