9b22a43f40e46e6855850b8b0ed8bd8907c11eb4
MK <mikakort007@gmail.com>
2025-06-25 03:16:49 -0400

    styling fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -405,7 +405,7 @@ export default function Home() {
                             objectFit="cover"
                             className="w-full h-full"
                           />
-                          <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-1">
+                          <div className="absolute bottom-2 left-1/2 -translate-x-1/2 flex gap-1">
                             {project.images.map((_, index) => (
                               <button
                                 key={index}
